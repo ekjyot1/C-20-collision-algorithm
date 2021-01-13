2f6c1a838fef7df2c2f404da2986a76066902fd1
ekjyot1 <75973617+ekjyot1@users.noreply.github.com>
2021-01-13 14:01:08 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,29 @@
+var fixedrect,movingrect;
+function setup() {
+  createCanvas(600,600);
+  fixedrect=createSprite(200, 200, 50, 80);
+  fixedrect.shapeColor="green";
+  movingrect=createSprite(00,200,80,30);
+  movingrect.shapeColor="green";
+}
+
+function draw() {
+  background("black"); 
+  movingrect.x=mouseX;
+  movingrect.y=mouseY; 
+
+  console.log(movingrect.x-fixedrect.x);
+  if(movingrect.x-fixedrect.x <=fixedrect.width/2+movingrect.width/2 &&
+    fixedrect.x-movingrect.x <=fixedrect.width/2+movingrect.width/2&&
+    movingrect.y-fixedrect.y<=fixedrect.height/2+movingrect.height/2&&
+    fixedrect.y-movingrect.y <=fixedrect.height/2+movingrect.height/2
+  ){
+  movingrect.shapeColor="red";
+  fixedrect.shapeColor="red";
+  }
+  else{
+    fixedrect.shapeColor="green";
+    movingrect.shapeColor="green"; 
+  }
+  drawSprites();
+}
